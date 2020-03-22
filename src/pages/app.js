54be5f63f2fda93a3d6ff36341382451ad6be3b3
Imran Sayed <sayedimrankashmiri@gmail.com>
2020-03-22 23:40:28 +0530

    Pages: Mens, Womens, Blogs and More pages

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,12 +2,14 @@ import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/Layout"
 import Shirts from "../components/mens/Shirts"
+import ShirtDetails from "../components/mens/ShirtDetails"
 
 const app = () => {
   return (
     <Layout>
       <Router>
         <Shirts path="/app/mens/shirts" />
+        <ShirtDetails path="/app/mens/shirt/:id" />
       </Router>
     </Layout>
   )
