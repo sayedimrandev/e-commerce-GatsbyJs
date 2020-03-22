150b37ac4f49b6fc2309aa171a21b5b6f95800c9
Imran Sayed <sayedimrankashmiri@gmail.com>
2020-03-22 23:41:37 +0530

    Components: Layout Component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import Arrow from "../components/Arrow"
 
 const Layout = ({ children }) => {
   return (
     <section>
       <Navbar />
-
       {children}
+      <Arrow />
       <Footer />
     </section>
   )
