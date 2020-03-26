8e91ad1a82b570a1c18f3b75e58693084a9b00cd
Imran Sayed <sayedimrankashmiri@gmail.com>
2020-03-26 23:22:44 +0530

    Components: Womens Component

---
+++ src/components/womens/JacketDetails.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react"
+import ClipLoader from "react-spinners/ClipLoader"
+import styles from "../../styles/mens/shirtdetail.module.css"
+
+const Fallback = ({ loading }) => {
+  return (
+    <section style={{ textAlign: `center` }}>
+      <ClipLoader size={100} color={"#123abc"} loading={loading} />
+    </section>
+  )
+}
+
+const JacketDetails = ({ id }) => {
+  const [jacket, setJacket] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`http://localhost:3000/womens/hoodies/${id}`)
+      const data = await response.json()
+      console.log(data.product)
+      setJacket(data.product)
+      setLoading(false)
+    }
+    getData()
+  }, [])
+
+  return (
+    <section className={styles.main}>
+      <h1 className={styles.heading}>Hoodie Details</h1>
+      {loading ? (
+        <Fallback loading={loading} />
+      ) : (
+        <section className={styles.container} key={jacket._id}>
+          <img src={jacket.Image} alt="productImage" />
+          <h1 className={styles.title}>{jacket.name}</h1>
+          <p className={styles.price}>{jacket.price}&#x20B9;</p>
+          <p className={styles.description}>{jacket.description}</p>
+        </section>
+      )}
+    </section>
+  )
+}
+
+export default JacketDetails
