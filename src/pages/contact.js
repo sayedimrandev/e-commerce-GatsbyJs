c5ed01637216964d67c930f3f99eaceff15018bf
Imran Sayed <sayedimrankashmiri@gmail.com>
2020-04-13 17:54:39 +0530

    Different Pages to navigate

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from "../components/Layout"
+import ContactUs from "../components/ContactUs"
+
+const more = () => {
+  return (
+    <section>
+      <React.Fragment>
+        <Layout>
+          <ContactUs />
+        </Layout>
+      </React.Fragment>
+    </section>
+  )
+}
+
+export default more
